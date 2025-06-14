5a234622801e2db11c3481339329c2b76eba4643
Peace-joy <163304783+Peace-joy@users.noreply.github.com>
2025-06-14 20:21:32 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,80 @@
+import time
+import matplotlib.pyplot as plt
+import visdom
+
+def convert_seconds_to_hms(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+    return hours, minutes, seconds
+
+from options.train_options import TrainOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import Visualizer
+
+if __name__ == '__main__':
+    opt = TrainOptions().parse()  # get training options
+    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    dataset_size = len(dataset)  # get the number of images in the dataset.
+    print('The number of training images = %d' % dataset_size)
+
+    model = create_model(opt)  # create a model given opt.model and other options
+    model.setup(opt)  # regular setup: load and print networks; create schedulers
+    visualizer = Visualizer(opt)  # create a visualizer that display/save images and plots
+    total_iters = 0  # the total number of training iterations
+
+    total_time_taken = 0
+
+    # Initialize Visdom
+    vis = visdom.Visdom(port=8097)  # Use port 8097 by default
+
+    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
+        epoch_start_time = time.time()
+        iter_data_time = time.time()
+        epoch_iter = 0
+        visualizer.reset()
+        model.update_learning_rate()
+
+        for i, data in enumerate(dataset):
+            iter_start_time = time.time()
+            if total_iters % opt.print_freq == 0:
+                t_data = iter_start_time - iter_data_time
+
+            total_iters += opt.batch_size
+            epoch_iter += opt.batch_size
+            model.set_input(data)
+            model.optimize_parameters()
+
+            if total_iters % opt.display_freq == 0:
+                save_result = total_iters % opt.update_html_freq == 0
+                model.compute_visuals()
+                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+
+            if total_iters % opt.print_freq == 0:
+                losses = model.get_current_losses()
+                t_comp = (time.time() - iter_start_time) / opt.batch_size
+                visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+                if opt.display_id > 0:
+                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+
+            if total_iters % opt.save_latest_freq == 0:
+                print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
+                save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
+                model.save_networks(save_suffix)
+
+            iter_data_time = time.time()
+
+        if epoch % opt.save_epoch_freq == 0:
+            print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
+            model.save_networks('latest')
+            model.save_networks(epoch)
+
+        epoch_time_taken = time.time() - epoch_start_time
+        total_time_taken += epoch_time_taken
+        print(
+            'End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, epoch_time_taken))
+
+        # Convert the total time taken into hours, minutes, and seconds
+        hours, minutes, seconds = convert_seconds_to_hms(total_time_taken)
+        print(f"Total Time Taken for all epochs so far: {hours} hours, {minutes} minutes, {seconds} seconds")
